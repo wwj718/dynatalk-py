6f68f078733bf26bf2a36eec8074d2ee9fbf1672
wwj718 <wuwenjie718@gmail.com>
2024-03-26 19:13:55 +0800

    update to 0.5.0(translated from dynatalk-js)

--- README.md
+++ README.md
@@ -14,4 +14,7 @@ from dynatalk import Supervisor, PyDemoAgent
 supervisor = Supervisor()
 agent = PyDemoAgent("PyDemoAgent")
 supervisor.addAgent(agent)
+
+agent.request("PyDemoAgent", "echo", ["hi"])
+# agent.request("LivelyDemoAgent", "add", [1, 2])
 ```
--- dynatalk/agent.py
+++ dynatalk/agent.py
@@ -1,5 +1,5 @@
 import uuid
-
+from concurrent.futures import Future, ThreadPoolExecutor
 
 class Agent:
     """
@@ -9,6 +9,7 @@ class Agent:
     def __init__(self, id, receive_own_broadcasts=False, debugging=False) -> None:
         self._RESPONSE_ACTION_NAME = "[response]"
         self._ERROR_ACTION_NAME = "[error]"
+        self._futures = {}
 
         self.supervisor = None
         self.id = id  # agent id
@@ -36,7 +37,64 @@ class Agent:
 
     def interpret(self, message):
         # The object interprets the message it understands
-        pass
+        agentName = message["action"]["name"]
+        agentID = message["meta"]["id"]
+        parent_id = message["meta"].get("parent_id")
+
+        if agentName == self._RESPONSE_ACTION_NAME:
+            #Handle incoming responses. Only useful when agent is used as callee
+            # console.debug("Handle incoming response", message);
+            # process _futures
+            future = self._futures.get(parent_id)
+            if future:
+                future.set_result(message["action"]["args"]["value"])
+                # del self._futures[parent_id]
+        elif agentName == self._ERROR_ACTION_NAME:
+            # console.debug("Handle incoming error", message);
+            future = self._futures.get(parent_id)
+            future.set_exception(Exception(message["action"]["args"]["error"]))
+        else:
+            # the caller requests the agent to execute the command
+            self._commit(message)
+
+    def _commit(self, message):
+        try:
+            # todo policy
+            action_method = getattr(self, message["action"]["name"], None)
+            if action_method:
+                action_method(*message["action"]["args"])
+            else:
+                error = f'Message Not Understood: {message["to"]}>>{message["action"]["name"]}'
+                self.raiseWith(error)
+                return
+        except Exception as e:
+            error = f'{self.id}>>{message["action"]["name"]} raised exception:' + str(e)
+            # console.error(error);
+            self.raiseWith(error)
+
+    def generateMessage(self, parent_id, to, action, args):
+        # Translated from dynatalk-js by chatgpt
+        message = {
+            "meta": {
+                "id": "",
+            },
+            "from": "",
+            "to": "",
+            "action": {
+                "name": "",
+                "args": "",
+            },
+        }
+        
+        if parent_id:
+            message["meta"]["parent_id"] = parent_id
+        message["meta"]["id"] = str(uuid.uuid4())
+        message["to"] = to
+        message["from"] = self.id
+        message["action"]["name"] = action
+        message["action"]["args"] = args
+        
+        return message
 
     def send(self, message):
         """
@@ -49,58 +107,63 @@ class Agent:
               The meta.id of the sent message
         """
 
-        message["from"] = self.id
-        if not ("meta" in message):
-            message["meta"] = {}
-        message["meta"]["id"] = str(uuid.uuid4())
         if self.debugging:
             self.debugLog(f"{self.id}: sending: {message}")
         # self._outbound_queue.put(message) // todo loop, now is directly
         self.supervisor.send(message)
         return message["meta"]["id"]
 
+    def _request(self, message, timeout=3):
+        # send and wait the response
+        # timeout 3000 ms
+        # The sync mechanism is implemented by the agent itself.
+        with ThreadPoolExecutor() as executor:
+            future = Future()
+            task = executor.submit(lambda f: f.result(timeout=timeout), future)
+            msg_id = self.send(message)
+            self._futures[msg_id] = future  # todo: threadsafe
+            return task.result()  # block and wait the result or TimeoutError
+
+    def request(self, agentName, actionName, args):
+        parent_id = None
+        message = self.generateMessage(parent_id, agentName, actionName, args)
+        return self._request(message)
+
+    def sendTo(self, agentName, actionName, args):
+        parent_id = None
+        message = self.generateMessage(parent_id, agentName, actionName, args)
+        return self.send(message)
+
     def respond_with(self, value):
         # Sends a response with the given value.
-        self.send(
-            {
-                "meta": {"parent_id": self.current_message["meta"]["id"]},
-                "to": self.current_message["from"],
-                "action": {
-                    "name": self._RESPONSE_ACTION_NAME,
-                    "args": {
-                        "value": value,
-                    },
-                },
-            }
-        )
+        parent_id = self.current_message["meta"]["id"]
+        to = self.current_message["from"]
+        action = self._RESPONSE_ACTION_NAME
+        args = {"value": value}
+        message = self.generateMessage(parent_id, to, action, args)
+    
+        self.send(message)
 
-    def raise_with(self, error):
+    def raiseWith(self, error):
         """
         Sends an error response.
 
         Args:
             error: The error to send.
         """
-        self.send(
-            {
-                "meta": {
-                    "parent_id": self.current_message["meta"]["id"],
-                },
-                "to": self.current_message["from"],
-                "action": {
-                    "name": self._ERROR_ACTION_NAME,
-                    "args": {"error": str(error)},
-                },
-            }
-        )
+        parent_id = self.current_message["meta"]["id"]
+        to = self.current_message["from"]
+        action = self._ERROR_ACTION_NAME
+        args = {"error": str(error)}
+        message = self.generateMessage(parent_id, to, action, args)
+    
+        self.send(message)
 
 
 class PyDemoAgent(Agent):
-    def interpret(self, message):
-        if message["action"]["name"] == "echo":
-            self.respond_with(message["action"]["args"][0])
-        else:
-            # Smalltalk style
-            self.raise_with(
-                f'Message Not Understood: {message["to"]}>>{message["action"]["name"]}'
-            )
+
+    def echo(self, content):
+        self.respond_with(content)
+
+    def add(self, a, b):
+        self.respond_with(a+b)
